9e2a5ea73178ca0754637f7acf383723aa335692
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-04 14:41:19 +0100

    FIX: Stylesheet paths

--- bambooSticks.rb
+++ bambooSticks.rb
@@ -29,8 +29,8 @@ gsub_file('Gemfile', /# gem 'redis'/, "gem 'redis'")
 ########################################
 run 'rm -rf app/assets/stylesheets'
 run 'rm -rf vendor'
-run 'curl -L https://github.com/mangotreedev/templates/archive/master.zip > stylesheets.zip'
-run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/templates-master app/assets/stylesheets'
+run 'curl -L https://github.com/mangotreedev/templates/stylesheets/archive/master.zip > stylesheets.zip'
+run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/stylesheets-master app/assets/stylesheets'
 
 # Dev environment
 ########################################
