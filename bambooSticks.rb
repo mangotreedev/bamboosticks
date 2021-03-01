210bda8c9ca7cf39162eae867956a30bcd817eed
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-28 21:51:07 -0500

    FIX: Added proper db config for db cleaner

--- bambooSticks.rb
+++ bambooSticks.rb
@@ -447,16 +447,38 @@ after_bundle do
     <<~RUBY
       # DatabaseCleaner with AR configuration
       config.before(:suite) do
-        DatabaseCleaner.strategy = :truncation
+        if config.use_transactional_fixtures?
+          raise(<<-MSG)
+            Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+            (or set it to false) to prevent uncommitted transactions being used in
+            JavaScript-dependent specs.
+
+            During testing, the app-under-test that the browser driver connects to
+            uses a different database connection to the database connection used by
+            the spec. The app's database connection would not be able to access
+            uncommitted transaction data setup over the spec's database connection.
+          MSG
+        end
         DatabaseCleaner.clean_with(:truncation)
       end
 
-      config.before do
+      config.before(:each) do
         DatabaseCleaner.strategy = :transaction
+      end
+
+      config.before(:each, type: :feature) do
+        driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+        unless driver_shares_db_connection_with_specs
+          DatabaseCleaner.strategy = :truncation
+        end
+      end
+
+      config.before(:each) do
         DatabaseCleaner.start
       end
 
-      config.append_after do
+      config.append_after(:each) do
         DatabaseCleaner.clean
       end
       # Factory Bot configuration
